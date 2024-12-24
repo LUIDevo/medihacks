7682fab4aa965032198aeab4c08c4306bfdbd4c3
Louis Berenyi <sanicisspeedandfast@gmail.com>
2024-12-23 22:57:44 -0500

    added port listening

--- server/index.js
+++ server/index.js
@@ -2,5 +2,13 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 app.use(express.json())
+
+
+
+
+app.listen(port, () => {
+    createTable();
+    console.log(`Server is running on http://localhost:${port}`);
+});
